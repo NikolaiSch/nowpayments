8d2025820de6e33e737d5f105262e02c117bc6e7
Nikolai Schimke <NikolaiSch@tuta.io>
2023-06-09 03:03:25 +0100

    added basic tests and sandbox

--- src/client.rs
+++ src/client.rs
@@ -1,6 +1,6 @@
 
 static BASE_URL: &str = "https://api.nowpayments.io/v1/{}";
-
+static BASE_SANDBOX_URL: &str = "https://api-sandbox.nowpayments.io/v1/{}";
 static USERAGENT: &str = concat!("rust/nowpayments/", "0.1.0");
 
 
@@ -13,10 +13,17 @@ pub struct NPClient {
 
 impl NPClient {
     pub fn new(api_key: impl ToString) -> Self {
-        dbg!(std::env::vars());
         Self {
             api_key: api_key.to_string(),
             base_url: BASE_URL,
+            client: reqwest::ClientBuilder::new().user_agent(USERAGENT).build().unwrap()
+        }
+    }
+
+    pub fn new_sandbox(api_key: impl ToString) -> Self {
+        Self {
+            api_key: api_key.to_string(),
+            base_url: BASE_SANDBOX_URL,
             client: reqwest::ClientBuilder::new().user_agent(USERAGENT).build().unwrap()
         }
     }
--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,39 @@
 pub mod client;
+
+#[cfg(test)]
+mod test {
+    use super::client::NPClient;
+    use envy;
+    use serde::{Deserialize, Serialize};
+
+    #[derive(Deserialize, Serialize)]
+    struct Config {
+        api: String,
+        sandboxapi: String,
+    }
+
+    fn parse_config() -> Config {
+        dotenvy::dotenv().unwrap();
+        envy::prefixed("NP_").from_env().unwrap()
+    }
+
+    fn client() -> NPClient {
+        let config = parse_config();
+        NPClient::new(config.api)
+    }
+
+    fn sandbox_client() -> NPClient {
+        let config = parse_config();
+        NPClient::new(config.sandboxapi)
+    }
+
+    #[test]
+    fn verify_client() {
+        client();
+    }
+
+    #[test]
+    fn verify_sandbox_client() {
+        sandbox_client();
+    }
+}
