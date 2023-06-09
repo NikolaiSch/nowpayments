05987adc43bc8d9ac14fadbc4c05b47d7ea02154
Nikolai Schimke <NikolaiSch@tuta.io>
2023-06-09 02:37:09 +0100

    moved 'NPClient' to seperate file

---
+++ src/client.rs
@@ -0,0 +1,23 @@
+
+static BASE_URL: &str = "https://api.nowpayments.io/v1/{}";
+
+static USERAGENT: &str = concat!("rust/nowpayments/", "0.1.0");
+
+
+pub struct NPClient {
+    api_key: String,
+    base_url: &'static str,
+
+    client: reqwest::Client
+}
+
+impl NPClient {
+    pub fn new(api_key: impl ToString) -> Self {
+        dbg!(std::env::vars());
+        Self {
+            api_key: api_key.to_string(),
+            base_url: BASE_URL,
+            client: reqwest::ClientBuilder::new().user_agent(USERAGENT).build().unwrap()
+        }
+    }
+}
